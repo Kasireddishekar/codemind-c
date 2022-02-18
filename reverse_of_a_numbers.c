2d998bce1107b6094d2940ee39fb1caab3546857
Kasireddishekar <98140510+Kasireddishekar@users.noreply.github.com>
2022-02-18 15:42:32 +0530

    #include<stdio.h> int main() { 	char str[100]; 	scanf("%[^ ]s",str); 	int i,j,t,len=0; 	for (i=0;str[i]!=NULL;i++) 	{ 		len=len+1; 	} 	i=0; 	i=len+1; 	i=0; 	j=len-1; 	while(i<j) 	{ 		t=str[j]; 		str[j]=str[i]; 		str[i]=t; 		i++; 		j--; 	} 	printf("%s",str); }

---
+++ reverse_of_a_numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	scanf("%[^
+]s",str);
+	int i,j,t,len=0;
+	for (i=0;str[i]!=NULL;i++)
+	{
+		len=len+1;
+	}
+	i=0;
+	i=len+1;
+	i=0;
+	j=len-1;
+	while(i<j)
+	{
+		t=str[j];
+		str[j]=str[i];
+		str[i]=t;
+		i++;
+		j--;
+	}
+	printf("%s",str);
+}
